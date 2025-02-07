33673e665470b2c019ab6b1b7aa3121164d2d5b0
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-07 15:05:03 +0530

    encode, decode string

---
+++ A_arrays_and_hashing/f_encode_decode_strings.py
@@ -0,0 +1,31 @@
+# Leetcode link - https://leetcode.com/problems/encode-and-decode-strings/
+
+class Solution:
+    # The main idea is to encode S1, S2, ..., Sn in the format <sizeofS1>#S1<sizeofS2>#S2....
+    # Decoding is done in similar reverse manner
+    # T: O(n) for encoding & decoding
+    # S: O(1) for encoding & decoding (not including final string)
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+
+        while i < len(s):
+            # get size of word
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            # get word
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            # update i
+            i = j
+        
+        return res
