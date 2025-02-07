6aed6fc2ae26941f5e3e0a7948b93533a0c807b6
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-07 16:57:15 +0530

    valid sudoku update

--- A_arrays_and_hashing/h_valid_sudoku.py
+++ A_arrays_and_hashing/h_valid_sudoku.py
@@ -25,5 +25,30 @@ class Solution:
                 squares[(r//3, c//3)].add(board[r][c])
 
         return True
-                
+    
+    # Bitmask based. T: O(n^2), S: O(n)
+    def isValidSudoku_bitmask(self, board: List[List[str]]) -> bool:
+        # Instead of storing multiple numbers 0-9 in the set for each index,
+        # we can store only one number whose bit position determines the actual number
+        rows = [0] * 9
+        cols = [0] * 9
+        squares = [0] * 9
         
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                val = int(board[r][c]) - 1
+                # Check if already exists - row, col, square
+                if (1 << val) & rows[r]:
+                    return False
+                if (1 << val) & cols[c]:
+                    return False
+                if (1 << val) & squares[(r // 3) * 3 + (c // 3)]:
+                    return False
+                
+                rows[r] |= (1 << val)
+                cols[c] |= (1 << val)
+                squares[(r // 3) * 3 + (c // 3)] |= (1 << val)
+        
+        return True
