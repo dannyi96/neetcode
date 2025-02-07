903b63e4527ff229c2417025161ed1eb3edfec23
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-07 16:27:27 +0530

    updates

--- A_arrays_and_hashing/g_product_of_array_except_self.py
+++ A_arrays_and_hashing/g_product_of_array_except_self.py
@@ -17,5 +17,29 @@ class Solution:
         
         return res
     
-    
+    # Division based with zero number handling. T: O(n), S:O(1)
+    def productExceptSelf_division(self, nums: List[int]) -> List[int]:
+        # Get total product(excluding 0s) & total 0 count
+        product = 1
+        zeroCount = 0
+        for num in nums:
+            if num:
+                product *= num
+            else:
+                zeroCount += 1
         
+        # if 2+ zeros, then final ans will be all 0s
+        if zeroCount >= 2:
+            return [0] * len(nums)
+
+        res = [0] * len(nums)
+        for index, num in enumerate(nums):
+            if zeroCount:
+                res[index] = 0 if num else product
+            else:
+                res[index] = product // num
+        return res
+        
+    # Prefix, Suffix based. T: O(n), S:O(n)
+    def productExceptSelf_division(self, nums: List[int]) -> List[int]:
+        pass
