4c9de62b403dbf99fec59d20b225a941317aa244
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-07 16:35:26 +0530

    updates

--- A_arrays_and_hashing/g_product_of_array_except_self.py
+++ A_arrays_and_hashing/g_product_of_array_except_self.py
@@ -41,5 +41,36 @@ class Solution:
         return res
         
     # Prefix, Suffix based. T: O(n), S:O(n)
-    def productExceptSelf_division(self, nums: List[int]) -> List[int]:
-        pass
+    def productExceptSelf_prefix_suffix(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * n
+        pref = [0] * n # Prefix array
+        suff = [0] * n # Suffix array
+        
+        pref[0] = suff[n-1] = 1
+        for i in range(1, n):
+            pref[i] = pref[i-1] * nums[i-1]
+        for i in range(n-2, -1, -1):
+            suff[i] = suff[i+1] * nums[i+1]
+        
+        for i in range(n):
+            res[i] = pref[i] * suff[i]
+        
+        return res
+    
+    # Prefix, Suffix based(Space optimised). T: O(n), S:O(1)
+    def productExceptSelf_prefix_suffix_optimised(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [1] * n
+        
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(n-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        
+        return res
