ef0fbbd08d5b1a3bd882146538b05ab12b4582fe
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-10 18:39:44 +0530

    trapping rain water added prefix suffix array method

--- B_two_pointers/e_trapping_rain_water.py
+++ B_two_pointers/e_trapping_rain_water.py
@@ -21,3 +21,26 @@ class Solution:
             res += min(leftMax, rightMax) - height[i]
         
         return res
+    
+    # Prefix, Suffix array based. T: O(n), S: O(n)
+    def trap_prefix_suffix_array(self, height: List[int]) -> int:
+        n = len(height)
+        leftMax = [0] * n
+        rightMax = [0] * n
+        
+        leftMax[0] = height[0]
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], height[i])
+        
+        rightMax[n - 1] = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            rightMax[i] = max(rightMax[i + 1], height[i])
+        
+        res = 0
+        for i in range(n):
+            res += min(leftMax[i], rightMax[i]) - height[i]
+        return res
+
+    # Stack based. T: O(n), S: O(n)
+    def trap_stack(self, height: List[int]) -> int:
+        pass
