91a3f1af8f7d7e7f213b64bbefbff6a7d70a4f44
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-14 18:00:20 +0530

    sliding window init

--- C_sliding_window/a_best_time_to_buy_sell_stock.py
+++ C_sliding_window/a_best_time_to_buy_sell_stock.py
@@ -1,2 +1,17 @@
 # Leetcode link - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+from typing import *
 
+class Solution:
+    # Bruteforce. T:O(n^2), S: O(1)
+    def maxProfit_bruteforce(self, prices: List[int]) -> int:
+        res = 0
+        for i in range(len(prices)):
+            buy = prices[i]
+            for j in range(i + 1, len(prices)):
+                sell  = prices[j]
+                res = max(res, sell - buy)
+        return res
+    
+    # Bruteforce. T:O(n^2), S: O(1)
+    def maxProfit_two_pointers(self, prices: List[int]) -> int:
+        pass
