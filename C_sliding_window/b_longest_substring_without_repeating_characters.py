c46280ed864db43e246f4ba541407c9bd89465a1
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-14 21:51:29 +0530

    longest substring no duplicates update

--- C_sliding_window/b_longest_substring_without_repeating_characters.py
+++ C_sliding_window/b_longest_substring_without_repeating_characters.py
@@ -2,7 +2,7 @@
 from typing import *
 
 class Solution:
-    # Bruteforce. T: O(n*m), S: O(m),
+    # Bruteforce. T: O(n*m), S: O(m)
     # - n is the length of the string and 
     # - m is the total number of unique characters in the string.
     def lengthOfLongestSubstring_bruteforce(self, s: str) -> int:
@@ -14,4 +14,36 @@ class Solution:
                     break
                 charSet.add(s[j])
             res = max(res, len(charSet))
+        return res
+    
+    # Sliding window approach. T: O(n), S: O(m)
+    def lengthOfLongestSubstring_sliding_window(self, s: str) -> int:
+        charSet = set()
+        res = 0
+        l = 0
+        
+        for r in range(len(s)):
+            while s[r] in charSet:
+                # Remove left character, incr l by 1
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r-l+1)
+        
+        return res
+    
+    # Sliding window approach. T: O(n), S: O(m)
+    def lengthOfLongestSubstring_sliding_window_optimised(self, s: str) -> int:
+        # Main idea - store index of duplicate char at r so we can directly set l
+        charIndexMap = {}
+        res = 0
+        l = 0
+        
+        for r in range(len(s)):
+            if s[r] in charIndexMap:
+                # directly set l to skip duplicate value occurance
+                l = max(charIndexMap[s[r]] + 1, l)
+            charIndexMap[s[r]] = r
+            res = max(res, r-l+1)
+        
         return res
