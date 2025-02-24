2dedbabc245cf984eda62cfb1273df0002c507a7
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-24 22:58:04 +0530

    polish notation

---
+++ D_stacks/c_reverse_polish_notation.py
@@ -0,0 +1,26 @@
+# Leetcode Link - https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+# Input: tokens = ["1","2","+","3","*","4","-"]
+# Output: 5
+# Explanation: ((1 + 2) * 3) - 4 = 5
+
+from typing import *
+
+class Solution:
+    # Stack based approach. T: O(n), S: O(n)
+    def evalRPN_stack(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b/a))
+            else:
+                stack.append(int(c))
+        return stack[0]
