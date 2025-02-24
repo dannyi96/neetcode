afd5489d95adbe55c0995ae63632e5b974b5ea4f
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-24 22:48:12 +0530

    stack - min stack optimal

--- D_stacks/b_min_stack.py
+++ D_stacks/b_min_stack.py
@@ -25,34 +25,25 @@ class MinStack:
 # Using Two stacks (Optimal - only push (elem, freq) into min stack)
 # T: O(1) for all operations, S: O(n)
 class MinStack:
-
     def __init__(self):
         self.stack = []
         self.min_stack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if self.min_stack:
-            if val < self.min_stack[-1][0]:
-                self.min_stack.append((val, 1))
-            else:
-                # greater than or equal to case
-                elem, elem_freq = self.min_stack[-1]
-                self.min_stack.pop()
-                self.min_stack.append((elem, elem_freq+1))
-        else:
+        if not self.min_stack or val < self.min_stack[-1][0]:
             self.min_stack.append((val, 1))
+        else:
+            self.min_stack[-1] = (self.min_stack[-1][0], self.min_stack[-1][1] + 1)
 
     def pop(self) -> None:
         if self.stack:
             self.stack.pop()
-            elem, elem_freq = self.min_stack[-1]
-            if elem_freq == 1:
+            if self.min_stack[-1][1] == 1:
                 self.min_stack.pop()
             else:
-                self.min_stack.pop()
-                self.min_stack.append((elem, elem_freq-1))
-        
+                self.min_stack[-1] = (self.min_stack[-1][0], self.min_stack[-1][1] - 1)
+
     def top(self) -> int:
         return self.stack[-1] if self.stack else -1
 
