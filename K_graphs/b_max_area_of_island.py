981b67ca4e05242ba79517b450c76e439e7036a4
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-26 11:54:05 +0530

    max area

---
+++ K_graphs/b_max_area_of_island.py
@@ -0,0 +1,26 @@
+# Leetcode link - https://leetcode.com/problems/max-area-of-island/
+from typing import *
+
+class Solution:
+    # DFS based. T: O(m*n), S: O(m*n)
+    def maxAreaOfIsland_dfs(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(r, c):
+            if (r < 0 or r == ROWS or c < 0 or
+                c == COLS or grid[r][c] == 0 or
+                (r, c) in visit
+            ):
+                return 0
+            visit.add((r, c))
+            return (1 + dfs(r + 1, c) + 
+                        dfs(r - 1, c) + 
+                        dfs(r, c + 1) + 
+                        dfs(r, c - 1))
+
+        area = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                area = max(area, dfs(r, c))
+        return area
