341873d6cda0c05fed4bde5b0ee6f9ba65faa2b3
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-26 13:45:27 +0530

    1d DP updates

---
+++ M_1d_dp/c_house_robber.py
@@ -0,0 +1,19 @@
+# Leetcode link - https://leetcode.com/problems/house-robber/
+from typing import *
+
+class Solution:
+    # T: O(n), S: O(n) --> can be space optimised to O(1)
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+        
+        return dp[-1]
