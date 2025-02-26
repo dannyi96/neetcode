8d84d2d41257f688109c363a3de58fccf7677e0a
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-26 13:39:08 +0530

    1d DP init

---
+++ M_1d_dp/b_min_cost_climbing_stairs.py
@@ -0,0 +1,14 @@
+# Leetcode link - https://leetcode.com/problems/min-cost-climbing-stairs/
+from typing import *
+
+class Solution:
+    # T: O(n), S: O(n) --> can be space optimised to O(1)
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0] * (n + 1)
+        
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1],
+                        dp[i - 2] + cost[i - 2])
+        
+        return dp[n]
