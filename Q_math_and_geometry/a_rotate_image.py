9beba81d04d53c173414ab41d26d5d2ad58ef638
Daniel Isaac <danielbcbs2@gmail.com>
2025-02-26 14:40:21 +0530

    math update

--- Q_math_and_geometry/a_rotate_image.py
+++ Q_math_and_geometry/a_rotate_image.py
@@ -1,1 +1,35 @@
 # Leetcode link - https://leetcode.com/problems/rotate-image/
+from typing import *
+
+class Solution:
+    def rotate_corners(self, matrix: List[List[int]]) -> None:
+        l, r = 0, len(matrix) - 1
+        while l < r:
+            for i in range(r - l):
+                top, bottom = l, r
+
+                # save the topleft
+                topLeft = matrix[top][l + i]
+
+                # move bottom left into top left
+                matrix[top][l + i] = matrix[bottom - i][l]
+
+                # move bottom right into bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+
+                # move top right into bottom right
+                matrix[bottom][r - i] = matrix[top + i][r]
+
+                # move top left into top right
+                matrix[top + i][r] = topLeft
+            r -= 1
+            l += 1
+
+    def rotate_reverse_transpose(self, matrix: List[List[int]]) -> None:
+        # Reverse the matrix vertically ( Last row becomes first & so on...)
+        matrix.reverse()
+
+        # Transpose the matrix
+        for i in range(len(matrix)):
+            for j in range(i + 1, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
